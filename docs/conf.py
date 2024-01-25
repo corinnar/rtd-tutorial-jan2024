5dd63e34e89b989d531e8a0614d672381e82a62d
corinnar <corinna.ravilious@unep-wcmc.org>
2024-01-25 20:53:35 +0000

    a

---
+++ docs/conf.py
@@ -0,0 +1,70 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Quizdown Demo'
+copyright = '2021, Malte Bonart'
+author = 'Malte Bonart'
+
+# The full version, including alpha/beta/rc tags
+release = '0.3.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinxcontrib.quizdown'
+]
+
+
+# global options passed to the quizdown library
+quizdown_config = {
+    'quizdown_js': 'https://cdn.jsdelivr.net/gh/bonartm/quizdown-js@latest/public/build/quizdown.js', # quizdown javascript
+    'start_on_load': True,			# detect and convert all divs with class quizdown
+    'shuffle_answers': True,		# shuffle answers for each question
+    'shuffle_questions': False,     # shuffle questsions for each quiz
+    'primary_color': '#FF851B',     # primary CSS color
+    'secondary_color': '#DDDDDD',   # secondary CSS color
+    'title_color': 'black'          # text color of the title
+}
+
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/quiz.md
@@ -0,0 +1,117 @@
+---
+shuffle_answers: true
+shuffle_questions: false
+locale: en
+---
+
+#### Who is the person in the picture?
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Sir_Tim_Berners-Lee.jpg/330px-Sir_Tim_Berners-Lee.jpg)
+
+> In 1990, he published the [worlds first website](http://info.cern.ch/hypertext/WWW/TheProject.html).
+
+- [x] Tim Berners-Lee
+- [ ] Alan Turing
+- [ ] Barbara Liskov
+- [ ] Larry Page
+
+
+#### Who is the person in the picture?
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Ted_Nelson_cropped.jpg/330px-Ted_Nelson_cropped.jpg)
+
+> He coined the terms *hyptertext* and *hypermedia* in 1963!
+
+- [x] Ted Nelson
+- [ ] Donald Knuth
+- [ ] Grace Hopper
+- [ ] John von Neumann
+
+
+#### What is the HTML element for the largest heading in a document?
+
+- [x] `<h1></h1>`
+- [ ] `<header></header>`
+- [ ] `<title></title>`
+- [ ] `<heading1></heading1>`
+
+
+
+#### What is the `<a></a>` tag used for?
+
+- [x] for defining hyperlinks
+- [ ] for defining a paragraph
+- [ ] for defining code snippet
+- [ ] for defining an image
+
+
+
+### What is the difference between a tag and an attribute?
+
+```html
+<a href="url">link text</a>
+```
+
+Select all correct answers:
+
+- [x] A tag represents an html element.
+- [x] An attribute defines a property of an html element.
+- [ ] An attribute is everything between the opening and closing tag.
+- [ ] `"url"` is an attribute in the example above.
+
+
+#### How many valid HTML element tags are currently defined?
+
+> Check out https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+
+- [x] 100 - 120
+- [ ] 80 - 100
+- [ ] 120 - 140
+- [ ] 140 - 160
+
+
+### How many `buttons` are defined on the [homepage of Spiced Academy](https://www.spiced-academy.com/en)?
+
+```html
+<button>
+    my button
+</button>
+```
+
+- [x] 7
+- [ ] 14
+- [ ] 5
+- [ ] 23
+
+
+#### What is the IP address of [google.com](https://google.com)?
+
+- [x] 142.250.186.110
+    > This is the ipv4 address
+- [x] 2a00:1450:4001:829::200e
+    > This is the ipv6 address
+- [ ] https
+- [ ] com
+
+
+#### What is a tag soup?
+
+> `<p>This is a tag <em>soup.</p></em>`
+
+- [ ] nicely written html code
+- [x] malformed html code
+
+
+
+#### Two organizations hide in the chunk of text. Do you find them?
+
+```
+CW3THAGWW
+```
+
+- [x] `W3C`
+    > World Wide Web Consortium
+- [x] `WHATWG`
+    > Web Hypertext Application Technology Working Group
+- [ ] `WWW`
+- [ ] `C3PO`
